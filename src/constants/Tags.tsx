91fcb33dd0b93bac11e1b0e18d21b97f507f2f4b
devalto <51037255+dev-alto@users.noreply.github.com>
2025-03-30 16:55:41 -0700

    feat: updated club-listing, [id] to use Supabase

--- src/constants/Tags.tsx
+++ src/constants/Tags.tsx
@@ -1,35 +1,33 @@
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
 import Entypo from '@expo/vector-icons/Entypo'
 
-import { ReactNode } from 'react'
+import { ReactElement } from 'react'
 
 interface TagProps {
     [key: string]: {
         color: string,
-        icon: ReactNode
+        icon: ReactElement
     }
 }
 
 const ICON_SIZE = 18
 
-export const Tags = {
-    STEM: 'STEM',
-    HOBBY: 'Hobby',
-    CULTURAL: 'Cultural'
-}
-
 export const TagDetails: TagProps = {
-    [Tags.STEM]: {
+    ['STEM']: {
         color: '#95DD31FF',
         icon: <MaterialCommunityIcons name='atom-variant' size={ICON_SIZE} color='white' />
     },
-    [Tags.HOBBY]: {
+    ['Hobby']: {
         color: '#FAA543FF',
         icon: <Entypo name='emoji-happy' size={ICON_SIZE} color='white' />
     },
-    [Tags.CULTURAL]: {
+    ['Cultural']: {
         color: '#8D59E6FF',
         icon: <MaterialCommunityIcons name='party-popper' size={ICON_SIZE} color='white' />
+    },
+    ['Unsorted']: {
+        color: '#4B4B4BFF',
+        icon: <MaterialCommunityIcons name='microscope' size={ICON_SIZE} color='white' />
     }
 }
 
