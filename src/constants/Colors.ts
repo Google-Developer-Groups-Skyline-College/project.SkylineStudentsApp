c5fc8f464d3d8037294f29d3ec9ab99f51dd32d2
devalto <51037255+dev-alto@users.noreply.github.com>
2025-03-30 16:58:23 -0700

    chore: `Colors.ts` constant file now uses tailwind classes

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -3,24 +3,26 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#FF3737FF';
-const tintColorDark = '#FF5959FF';
+const tintColorLight = '#FF3737FF'
+const tintColorDark = '#FF5959FF'
 
 export const Colors = {
   light: {
-    text: '#11181CFF',
-    background: '#fff',
+    text: 'text-black',
+    background: 'bg-white',
+    cardBackground: 'bg-neutral-200',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: 'text-white',
+    background: 'bg-[#111111]',
+    cardBackground: 'bg-neutral-800',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
-  },
-};
+  }
+}
