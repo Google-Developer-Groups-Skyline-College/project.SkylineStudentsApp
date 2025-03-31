f3764fd6e6037e1a74c7c9c4a154ee6c93c84016
devalto <51037255+dev-alto@users.noreply.github.com>
2025-03-30 17:04:59 -0700

    feat: new base `Image` component, integrates base fallbacks, transitions, cssInterop

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,10 +1,36 @@
-import { Image } from 'expo-image'
+import { Image as ExpoImage, ImageProps as ExpoImageProps } from 'expo-image'
+import { ImageProps as NativeImageProps } from 'react-native'
 import { cssInterop } from 'nativewind'
 
-cssInterop(Image, { className: "style" })
+const InterOpImage = cssInterop(ExpoImage, { className: 'style' })
 
-export default function({...props}) {
-    return (
-        <Image {...props}></Image>
-    )
+const SOURCE_PLACEHOLDER = require('$/images/image.placeholder.webp')
+const SOURCE_FALLBACK = require('$/images/image.missing.webp')
+
+type ImageProps = ExpoImageProps & NativeImageProps
+
+export default function Image({source = SOURCE_FALLBACK, ...props}: ImageProps) {
+    return <InterOpImage
+        source={source}
+        placeholder={SOURCE_PLACEHOLDER}
+        placeholderContentFit='contain'
+        transition={{ duration: 500 }}
+        {...props}
+    />
 }
+
+// import { Image as NativeImage, ImageSourcePropType } from 'react-native'
+// import { Image as ExpoImage, ImageSource } from 'expo-image'
+
+// import { cssInterop } from 'nativewind'
+
+// cssInterop(ExpoImage, { className: 'style' })
+
+// const SOURCE_FALLBACK = require('$/images/missing.webp')
+
+// export default function Image({className, source = SOURCE_FALLBACK, native, ...props}: { className: string, source: ImageSourcePropType | ImageSource | string, native?: boolean } & ExpoImage) {
+//     if (native) {
+//         return <NativeImage source={source as ImageSourcePropType} className={className} {...props} />
+//     }
+//     return <ExpoImage source={source as ImageSource} className={className} {...props} />
+// }
