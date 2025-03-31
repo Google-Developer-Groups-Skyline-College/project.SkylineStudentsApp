d8146cc3c7fbf907ed72046b2550ce7d5d5bfcf4
devalto <51037255+dev-alto@users.noreply.github.com>
2025-03-30 17:09:46 -0700

    feat: `VideoCard` pressable extension of `Card` with embedded video and press-to-play overlay

---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,14 @@
+import { ThemedViewProps } from './ThemedView'
+import Image from './Image'
+import Card from './Card'
+
+interface VideoCardProps extends ThemedViewProps {
+    source: string
+}
+
+export default function VideoCard({ source, ...otherProps }: VideoCardProps) {
+    return <Card {...otherProps}>
+        <Image source={source} className='w-full h-full'></Image>
+        <Image source={require('$/images/videocard.player-overlay.webp')} contentFit='contain' className='absolute w-full h-full'></Image>
+    </Card>
+}
