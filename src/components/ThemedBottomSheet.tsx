6c8061a7941b8f6ebd3bfd59d48359eddb242813
devalto <51037255+dev-alto@users.noreply.github.com>
2025-03-30 17:07:45 -0700

    feat: `ThemedBottomSheet` component, themable, cssInterop, extension of @gorhom's `BottomSheet`

---
+++ src/components/ThemedBottomSheet.tsx
@@ -0,0 +1,46 @@
+import { forwardRef } from 'react'
+import BottomSheet, { BottomSheetProps } from '@gorhom/bottom-sheet'
+
+import { useThemeColor } from '@/hooks/useThemeColor'
+import { cssInterop } from 'nativewind'
+
+const InterOpBottomSheet = cssInterop(BottomSheet, {
+    backgroundClassName: {
+        target: 'backgroundStyle'
+    }
+})
+
+interface ThemedBottomSheetProps extends BottomSheetProps {
+    backgroundClassName?: string
+}
+
+export default forwardRef<BottomSheet, ThemedBottomSheetProps>(function ThemedBottomSheet(props, ref) {
+    const backgroundColor = useThemeColor(null, 'background')
+
+    return <InterOpBottomSheet ref={ref} backgroundClassName={`${props.backgroundClassName} ${backgroundColor}`} {...props} />
+})
+
+// alternative semi-working implementation, not using forwardRef
+
+// import { Ref } from 'react'
+// import BottomSheet , { BottomSheetProps } from '@gorhom/bottom-sheet'
+
+// import { useThemeColor } from '@/hooks/useThemeColor'
+// import { cssInterop } from 'nativewind'
+
+// const InterOpBottomSheet = cssInterop(BottomSheet, {
+//     backgroundClassName: {
+//         target: 'backgroundStyle',
+//     }
+// })
+
+// interface ThemedBottomSheetProps extends BottomSheetProps {
+//     backgroundClassName?: string
+//     ref: Ref<BottomSheet>
+// }
+
+// export default function ThemedBottomSheet({ ref, backgroundClassName, ...otherProps }: ThemedBottomSheetProps) {
+//     const backgroundColor = useThemeColor(null, 'background')
+
+//     return <InterOpBottomSheet ref={ref} backgroundClassName={`${backgroundClassName} ${backgroundColor}`} {...otherProps} />
+// }
