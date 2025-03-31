2cb77940d1b3be2fbc453f688c26ff6e25e7df15
devalto <51037255+dev-alto@users.noreply.github.com>
2025-03-30 17:12:47 -0700

    feat: mapbox component abstraction

---
+++ src/app/map/components/PointOfInterestMarker.tsx
@@ -0,0 +1,30 @@
+import { Pressable, View, Text } from 'react-native'
+
+import Ionicons from '@expo/vector-icons/Ionicons'
+
+import { PointOfInterest } from '..'
+import { useEffect, useState } from 'react'
+
+export interface PointOfInterestMarkerProps {
+    feature: PointOfInterest
+    onPressed: (feature: PointOfInterest) => void
+}
+
+export default function PointOfInterestMarker({ feature, onPressed }: PointOfInterestMarkerProps) {
+
+    const [ fadeIn, setFadeIn ] = useState(false)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setFadeIn(true)
+        }, Math.random() * 500)
+    }, [])
+
+    return (
+        <Pressable className={`flex flex-row justify-center active:opacity-50 transition-opacity duration-300 ${fadeIn ? 'opacity-100' : 'opacity-0'}`} onPress={() => {onPressed(feature)}}>
+            <View className='px-2 py-1 rounded-xl bg-neutral-900/75'>
+                <Text className='text-white text-sm'>{feature.name}</Text>
+            </View>
+        </Pressable>
+    )
+}
