1147e2069148d3cc3906973805377bc098d9565d
devalto <51037255+dev-alto@users.noreply.github.com>
2025-03-30 17:00:54 -0700

    feat: new `HorizontalRule` component

---
+++ src/components/HorizontalRule.tsx
@@ -0,0 +1,15 @@
+import { View, PixelRatio, useColorScheme } from 'react-native'
+import invert from 'invert-color'
+
+const pixelRatio = 1 / PixelRatio.get()
+
+export default function HorizontalRule({ height = 1, color = '#EEE' }) {
+    const theme = useColorScheme()
+    if (theme === 'light')
+        color = invert(color)
+
+    return <View className='w-full' style={{
+        height: pixelRatio * height,
+        backgroundColor: color
+    }} />
+}
