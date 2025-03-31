3691a469dd0a4dd834437c497784b814b181745d
devalto <51037255+dev-alto@users.noreply.github.com>
2025-03-30 17:08:43 -0700

    feat: new base `Video` component, used to easily embedded useVideoPlayer hook

---
+++ src/components/Video.tsx
@@ -0,0 +1,17 @@
+import { VideoView, VideoPlayer, useVideoPlayer } from 'expo-video'
+import { ViewProps } from 'react-native'
+
+import { cssInterop } from 'nativewind'
+
+const InterOpVideoView = cssInterop(VideoView, { className: 'style'})
+
+interface VideoProps extends ViewProps {
+    source: string
+    setup?: (player: VideoPlayer) => void
+}
+
+export default function Video({source, setup, ...otherProps }: VideoProps) {
+    const player = useVideoPlayer(source, setup ? (player => setup(player)) : undefined)
+
+    return <InterOpVideoView player={player} {...otherProps} />
+}
